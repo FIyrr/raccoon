3ea13c6f40edcb79f1e936c76760d808efe25317
Flynecraft <flynn.kresslein@gmx.de>
2024-04-27 10:33:13 +0200

    test

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
 window.onload = function() {
     const image_id = randomIntFromInterval(1, 3);
 
-    // Get a reference to the image element
     var img = document.getElementById("randomImage");
 
-    // Set the source of the image to a random image from the dataset
     img.src = "images/" + image_id + ".jpg";
 };
